b952979d8e270983964618757a385f0fb59c48b9
Ryan Beisner <ryan.beisner@canonical.com>
2017-03-02 16:17:34 -0600

    Update runner

--- runners/openstack-base-arm64/run.sh
+++ runners/openstack-base-arm64/run.sh
@@ -1,34 +1,41 @@
 #!/bin/bash -ex
 #
-# Example test runner for ARM64 OpenStack-Base bundle deploy on MAAS
+# An example test runner for ARM64 OpenStack-Base bundle deploy on MAAS
+#
+# Default environment variable values represent a specific lab and must
+# be adjusted to suit.
+#
 
 ## Tool env vars
-export JUJU_WAIT_CODIR="$HOME/temp/juju-wait"
-export JUJU_WAIT_CMD="time timeout 45m $JUJU_WAIT_CODIR/juju-wait -v"
+: ${JUJU_WAIT_CODIR:="$HOME/temp/juju-wait"}
+: ${JUJU_WAIT_CMD:="time timeout 45m $JUJU_WAIT_CODIR/juju-wait -v"}
+
+: ${OCT_CODIR:="$HOME/temp/openstack-charm-testing"}
+: ${OCT_CONFIGURE_CMD:="$OCT_CODIR/configure dellstack"}
 
 ## Controller env vars
-export CONTROLLER_NAME="ruxton-maas"
-export BOOTSTRAP_CONSTRAINTS="arch=arm64 tags=gigabyte"
-export MODEL_CONSTRAINTS="$BOOTSTRAP_CONSTRAINTS"
+: ${CONTROLLER_NAME:="ruxton-maas"}
+: ${BOOTSTRAP_CONSTRAINTS:="arch=arm64 tags=gigabyte"}
+: ${MODEL_CONSTRAINTS:="$BOOTSTRAP_CONSTRAINTS"}
 
 ## Model env vars
-export MODEL_NAME="openstack-base-arm64"
-export CONTROLLER_NAME="ruxton-maas"
-export OPENSTACK_BUNDLES_CODIR="$HOME/temp/openstack-bundles"
-export CTI_CODIR="$HOME/temp/charm-test-infra"
+: ${MODEL_NAME:="openstack-base-arm64"}
+: ${CONTROLLER_NAME:="ruxton-maas"}
+: ${OPENSTACK_BUNDLES_CODIR:="$HOME/temp/openstack-bundles"}
+: ${CTI_CODIR:="$HOME/temp/charm-test-infra"}
 
 ## Bundle env vars
-export DATA_PORT_INTERFACE="enP2p1s0f2"
-export REF_BUNDLE_FILE="development/openstack-base-xenial-mitaka/bundle.yaml"
-export BUNDLE_FILE="$(mktemp)"
+: ${DATA_PORT_INTERFACE:="enP2p1s0f2"}
+: ${REF_BUNDLE_FILE:="development/openstack-base-xenial-mitaka/bundle.yaml"}
+: ${BUNDLE_FILE:="$(mktemp)"}
 
 ## Bootstrap if not bootstrapped
 juju switch $CONTROLLER_NAME ||\
-    juju bootstrap --bootstrap-constraints="$BOOTSTRAP_CONSTRAINTS" \
-                   --auto-upgrade=false \
-                   --bootstrap-series=xenial \
-                   --model-default=$CTI_CODIR/juju-configs/model-default.yaml \
-                   $CONTROLLER_NAME
+    time juju bootstrap --bootstrap-constraints="$BOOTSTRAP_CONSTRAINTS" \
+                       --auto-upgrade=false \
+                       --bootstrap-series=xenial \
+                       --model-default=$CTI_CODIR/juju-configs/model-default.yaml \
+                       $CONTROLLER_NAME
 
 ## Add model if it doesn't exist
 juju switch ${CONTROLLER_NAME}:${MODEL_NAME} ||\
@@ -47,6 +54,7 @@ time timeout 90m juju deploy -m ${CONTROLLER_NAME}:${MODEL_NAME} $BUNDLE_FILE
 $JUJU_WAIT_CMD
 
 ## Configure
+$OCT_CONFIGURE_CMD
 
 ## Test
 
