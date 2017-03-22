32401a3a98a45f21bc13e95d97b71f64767de5f3
Ryan Beisner <ryan.beisner@canonical.com>
2017-03-22 14:47:26 -0500

    Update runner

--- runners/openstack-base-arm64/run.sh
+++ runners/openstack-base-arm64/run.sh
@@ -6,11 +6,18 @@
 # be adjusted to suit.  These default values will only take effect if an
 # environment variable is not already set.
 
+## Per-job env vars
+: ${UBUNTU_RELEASE:="xenial"}
+: ${OPENSTACK_RELEASE:="ocata"}
+: ${BUNDLE_STABILITY:="development"}
+: ${BUNDLE_SCENARIO:="openstack-base"}
+
 ## Tool env vars
 : ${JUJU_WAIT_CODIR:="$HOME/temp/juju-wait"}
 : ${OCT_CODIR:="$HOME/temp/openstack-charm-testing"}
 : ${BC_CODIR:="$HOME/temp/bot-control"}
-
+: ${BUNDLE_CODIR:="$HOME/temp/openstack-bundles"}
+: ${CTI_CODIR:="$HOME/temp/charm-test-infra"}
 
 : ${JUJU_WAIT_CMD:="time timeout 60m $JUJU_WAIT_CODIR/juju-wait -v"}
 : ${CONFIGURE_CMD:="./configure arm64"}
@@ -19,26 +26,27 @@
 : ${CLOUD_NAME:="ruxton-maas"}
 
 ## Controller env vars
-: ${CONTROLLER_NAME:="ruxton-maas-arm64"}
+: ${CONTROLLER_NAME:="${CLOUD_NAME}-arm64"}
 : ${BOOTSTRAP_CONSTRAINTS:="arch=arm64 tags=gigabyte"}
 
 ## Model env vars
-: ${MODEL_NAME:="openstack-base-arm64"}
+: ${MODEL_NAME:="${BUNDLE_SCENARIO}-${UBUNTU_RELEASE}-${OPENSTACK_RELEASE}-${BUNDLE_STABILITY}"}
 : ${MODEL_CONSTRAINTS:="$BOOTSTRAP_CONSTRAINTS"}
-: ${OPENSTACK_BUNDLES_CODIR:="$HOME/temp/openstack-bundles"}
-: ${CTI_CODIR:="$HOME/temp/charm-test-infra"}
 
 ## Bundle env vars
 : ${DATA_PORT_INTERFACE:="enP2p1s0f2"}
-: ${REF_BUNDLE_FILE:="development/openstack-base-xenial-mitaka/bundle.yaml"}
+: ${REF_BUNDLE_FILE:="${BUNDLE_STABILITY}/${BUNDLE_SCENARIO}-${UBUNTU_RELEASE}-${OPENSTACK_RELEASE}/bundle.yaml"}
 : ${BUNDLE_FILE:="$(mktemp)"}
 
 ## Fixture env vars
 : ${TEST_IMAGE_URL_XENIAL:="http://10.245.161.162/swift/v1/images/xenial-server-cloudimg-arm64-uefi1.img"}
 
+## Gather tools if not present
+    # NOT YET IMPLEMENTED
+
 ## Add cloud if not present
 juju show-cloud ${CLOUD_NAME} ||\
-    # NOT IMPLEMENTED
+    # NOT YET IMPLEMENTED
     exit 1
 
 ## Bootstrap if not bootstrapped
@@ -56,7 +64,7 @@ juju switch ${CONTROLLER_NAME}:${MODEL_NAME} ||\
 juju set-model-constraints -m $MODEL_NAME "$MODEL_CONSTRAINTS"
 
 ## Fetch and modify bundle
-cp -fvp $OPENSTACK_BUNDLES_CODIR/$REF_BUNDLE_FILE $BUNDLE_FILE
+cp -fvp $BUNDLE_CODIR/$REF_BUNDLE_FILE $BUNDLE_FILE
 sed -e "s/data-port: br-ex:eth1/data-port: br-ex:${DATA_PORT_INTERFACE}/g" -i $BUNDLE_FILE
 
 ## Deploy
@@ -74,6 +82,7 @@ openstack --version
 cd $HOME
 
 
+
 exit 0
 
 
@@ -87,7 +96,10 @@ cd $HOME
 cd $OCT_CODIR
 ./instance_launch.sh 6 xenial-uefi
 cd $HOME
+    # TODO: Run tempest tests
 
 ## Collect
+    # NOT YET IMPLEMENTED
 
 ## Destroy
+    # NOT YET IMPLEMENTED
