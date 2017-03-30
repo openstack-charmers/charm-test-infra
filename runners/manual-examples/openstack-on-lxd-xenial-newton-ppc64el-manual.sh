def3327852acd0c53f0ca75ef06b050091371398
Ryan Beisner <ryan.beisner@canonical.com>
2017-03-30 02:11:15 -0500

    Add oolxd ppc64el example manual scripts

---
+++ runners/manual-examples/openstack-on-lxd-xenial-newton-ppc64el-manual.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+# This example can be used to manually run a scenario.  Some edits and
+# customizations may be necessary to match a specific lab.
+#
+# Typically, the variables set here are representative of the variables
+# which would normally be passed from Jenkins jobs to the runner.
+
+# huffman ppc64el
+export REMOTE=10.245.168.45  # Temp IP!
+export BUNDLE_FILE="bundle-newton.yaml"
+export WORKSPACE="/tmp/WORKSPACE-HUFFMAN"
+export ZPOOL_DEVS="/dev/mpatha /dev/mpathb /dev/mpathc"  # Disk order danger!
+
+openstack-on-lxd/run.sh
