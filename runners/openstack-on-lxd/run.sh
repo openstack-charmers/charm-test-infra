5c1e8ab6ea81f59d5e851920850ee57cfd9052e3
Ryan Beisner <ryan.beisner@canonical.com>
2017-03-31 14:43:25 -0500

    Update oolxd runner for updated neutron-ext-net

--- runners/openstack-on-lxd/run.sh
+++ runners/openstack-on-lxd/run.sh
@@ -198,7 +198,7 @@ EOF_IMAGE_CREATE
 rexec << EOF_NETWORK_CONFIGURE
   $NOVARC_CMD
   cd $REMOTE_WORKSPACE/openstack-on-lxd
-  ./neutron-ext-net -g 10.0.8.1 -c 10.0.8.0/24 -f 10.0.8.201:10.0.8.254 ext_net
+  ./neutron-ext-net --network-type flat -g 10.0.8.1 -c 10.0.8.0/24 -f 10.0.8.201:10.0.8.254 ext_net
   ./neutron-tenant-net -t admin -r provider-router -N 10.0.8.1 internal 192.168.20.0/24
   cd
 EOF_NETWORK_CONFIGURE
