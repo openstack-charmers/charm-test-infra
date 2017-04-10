ed8cfd34ed333dc710bba6372554738543438b2e
Ryan Beisner <ryan.beisner@canonical.com>
2017-04-10 12:06:30 -0500

    Add juju controller destroy example

---
+++ juju-destroy-controller-and-models-example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# OpenStack Tenant User - Juju Controller and Example Model Destruction
+
+if [ -z "${OS_PROJECT_NAME}" ]; then
+  echo "ERROR: Have you sourced novarc?"
+  exit 1
+fi
+
+set -ux
+
+export CLOUD_NAME="$OS_REGION_NAME"
+export CONTROLLER_NAME="${OS_PROJECT_NAME}-${CLOUD_NAME}"
+
+juju destroy-controller --destroy-all-models $1 $CONTROLLER_NAME
