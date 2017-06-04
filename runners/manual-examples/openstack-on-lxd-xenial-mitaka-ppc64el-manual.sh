e06af0dcfff22a915d27767800b04aae71c2ed36
Ryan Beisner <ryan.beisner@canonical.com>
2017-06-04 13:34:25 -0500

    Update manual runner examples

--- runners/manual-examples/openstack-on-lxd-xenial-mitaka-ppc64el-manual.sh
+++ runners/manual-examples/openstack-on-lxd-xenial-mitaka-ppc64el-manual.sh
@@ -5,7 +5,7 @@
 # Typically, the variables set here are representative of the variables
 # which would normally be passed from Jenkins jobs to the runner.
 
-export REMOTE=10.245.168.39  # Temp IP! gengar
+export REMOTE=10.245.168.40  # Temp IP! huffman
 export BUNDLE_FILE="bundle-mitaka.yaml"
 export WORKSPACE="/tmp/WORKSPACE-$REMOTE"
 export ZPOOL_DEVS="/dev/sda /dev/sdb /dev/sdc /dev/sde"  # Disk order danger!
--- runners/manual-examples/openstack-on-lxd-xenial-ocata-ppc64el-manual.sh
+++ runners/manual-examples/openstack-on-lxd-xenial-ocata-ppc64el-manual.sh
@@ -5,7 +5,7 @@
 # Typically, the variables set here are representative of the variables
 # which would normally be passed from Jenkins jobs to the runner.
 
-export REMOTE=10.245.168.47  # Temp IP! huffman
+export REMOTE=10.245.168.39  # Temp IP! gengar
 export BUNDLE_FILE="bundle-ocata.yaml"
 export WORKSPACE="/tmp/WORKSPACE-$REMOTE"
 export ZPOOL_DEVS="/dev/sdb /dev/sdc /dev/sdd /dev/sde"  # Disk order danger!
