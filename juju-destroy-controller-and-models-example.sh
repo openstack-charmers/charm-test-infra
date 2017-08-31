8701144b9980d4758509469b09182efc0edfdb68
Ryan Beisner <ryan.beisner@canonical.com>
2017-08-31 16:54:16 -0500

    Resort to kill-controller when necessary

--- juju-destroy-controller-and-models-example.sh
+++ juju-destroy-controller-and-models-example.sh
@@ -16,7 +16,13 @@ set -ux
 : ${CLOUD_NAME:="$OS_REGION_NAME"}
 : ${CONTROLLER_NAME:="${OS_PROJECT_NAME}-${CLOUD_NAME}"}
 
+if ! juju controllers | grep 'auto-osci-sv07.*superuser'; then
+    exit 0
+fi
 
-juju switch ${CONTROLLER_NAME}:controller &&\
-    juju destroy-controller --destroy-all-models $1 $CONTROLLER_NAME ||\
-        echo "Cloud/Controller not found (${CLOUD_NAME} ${CONTROLLER_NAME})"
+juju switch ${CONTROLLER_NAME}:controller
+
+if ! juju destroy-controller --destroy-all-models $1 $CONTROLLER_NAME; then
+    juju kill-controller $1 $CONTROLLER_NAME
+fi
+
