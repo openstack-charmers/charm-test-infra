20c2d2ffd73bd1c66b4aae68bcb9c94272039675
Ryan Beisner <ryan.beisner@canonical.com>
2017-11-28 17:10:43 +0000

    Update refstack example

--- examples/go-refstack.sh
+++ examples/go-refstack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
+#
+# This is experimental and still needs work.  Saving as a WIP/reference.
 
 # Require these checkouts
 stat -t $HOME/temp/charm-test-infra
@@ -9,25 +11,26 @@ cd $HOME/temp
 stat -t refstack-client ||\
     git clone https://github.com/openstack/refstack-client --depth 1
 
-# Use alternate setup which requires no sudo
+# Set up refstack venv using alternate setup which does not require sudo or apt
 cp -fv $HOME/temp/charm-test-infra/fixtures/refstack/setup_venv_no_sudo refstack-client
 cd refstack-client
-./setup_venv_no_sudo
+stat .tempest ||\
+    ./setup_venv_no_sudo
 
-# Build venv
+# Build clients venv for tempest template render
 deactivate ||:
-stat -t refstack-venv ||\
-    ln -s .venv/bin/activate refstack-venv
-. refstack-venv
-pip install -r $HOME/temp/charm-test-infra/clients-requirements.txt
-openstack --version
-
-# Render tempest template
+cd $HOME/temp/charm-test-infra
+tox
+. $HOME/temp/charm-test-infra/clientsrc
 cd $HOME/temp/openstack-charm-testing
 ./configure refstack_only
 cp -fv $HOME/temp/openstack-charm-testing/tempest_refstack.conf $HOME/temp/refstack-client/tempest.conf
 
 # One short test first, then go for the full refstack run
 cd $HOME/temp/refstack-client
+deactivate ||:
+stat -t refstack-venv ||\
+    ln -s .venv/bin/activate refstack-venv
+. refstack-venv
 time ./refstack-client test -c tempest.conf -v -- --regex TokensV3Test
 time ./refstack-client test -c tempest.conf -v
