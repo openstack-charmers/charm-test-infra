a70c64b7768a7923ab18e328814f1e5fba034e33
Andrew McLeod <andrew.mcleod@canonical.com>
2020-01-23 16:44:31 +0100

    fix

--- juju-openstack-controller-mosci.sh
+++ juju-openstack-controller-mosci.sh
@@ -54,7 +54,7 @@ juju controller-config features="[multi-cloud]"
 #juju add-cloud --replace $CLOUD_NAME juju-configs/clouds.yaml
 juju update-cloud $CLOUD_NAME -f juju-configs/clouds.yaml --client
 
-juju autoload-credentials --client --controller $CONTROLLER_NAME <<EOF
+juju autoload-credentials --client <<EOF
 1
 overcloud
 q
